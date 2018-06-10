2b4bf572c3586bb8bff0c875765435974e93b38a
Rauli Leevi Sakari Puuperä <therauli@therau.li>
2018-06-10 21:05:22 +0300

    Added better fromatting for distance

--- Venue Searcher/Views/VenueCell.swift
+++ Venue Searcher/Views/VenueCell.swift
@@ -26,10 +26,17 @@ class VenueCell: UITableViewCell {
     
     func setData(venue: Venue, distance: Double) {
         self.venueLabel.text = venue.name
-        
-        let formatted = NSString(format: "%.1fm", distance)
-
-        self.distanceLabel.text = String(formatted)
+        self.distanceLabel.text = format(distance: distance)
+    }
+    
+    private func format(distance: Double) -> String {
+        switch distance {
+        case 0.0 ..< 1000.0:
+            return String(NSString(format: "%.1fm", distance))
+        default:
+            return String(NSString(format: "%.1fkm", distance / 1000.0))
+        }
+       
     }
 
-}
+}   
